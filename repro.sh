a1c303daad99183d31599f4e3b103e55aef75e15
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-02-04 01:20:50 +0100

    wip

--- repro.sh
+++ repro.sh
@@ -34,12 +34,15 @@ fi
 sleep 5
 ./bin/workload run tpcc --expensive-checks --scatter --warehouses $warehouses --ramp 30s --wait=false --tolerate-errors --duration=24h 'postgres://root@localhost:26257?sslmode=disable' --histograms tpcc.json &
 
+sleep 5
+
 while true; do
 #ALTER TABLE tpcc.district SPLIT AT SELECT i FROM generate_series(1, $((10*warehouses-1))) AS g(i);
 #ALTER TABLE tpcc.district SCATTER;
 	./cockroach sql --insecure -e "
 SET CLUSTER SETTING kv.range_merge.queue_enabled = false;
 ALTER TABLE tpcc.warehouse SPLIT AT SELECT i FROM generate_series(1, $((warehouses-1))) AS g(i);
+	sleep 30
 SET CLUSTER SETTING kv.range_merge.queue_enabled = true;
 ALTER TABLE tpcc.warehouse SCATTER;
 "
